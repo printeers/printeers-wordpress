90ba46116f19e9ecb42388bf9bb291fc6b25d344
Mike Sies <23238930+MikeSies@users.noreply.github.com>
2026-07-15 10:55:42 +0200

    Add uninstall cleanup, i18n wrappers and HPOS declaration

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+$printeers_key_id = (int) get_option( 'printeers_api_key_id', 0 );
+if ( $printeers_key_id ) {
+	global $wpdb;
+	$wpdb->delete( $wpdb->prefix . 'woocommerce_api_keys', array( 'key_id' => $printeers_key_id ) );
+}
+
+delete_option( 'printeers_connected' );
+delete_option( 'printeers_store_url' );
+delete_option( 'printeers_api_key_id' );
+delete_option( 'printeers_connect_nonce' );
